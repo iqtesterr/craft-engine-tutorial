dd6c3525d488330bb7434374c5aa8a78477b35cf
iqtester <1835ww@gmail.com>
2026-05-27 00:14:48 -0400

    setup

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'XiaoQMi Wiki',
+  tagline: 'idk what is this',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://wiki.xiaoqmi.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'XiaoQMi', // Usually your GitHub org/user name.
+  projectName: 'wikis', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -32,7 +32,7 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en', 'zh-Hans'],
   },
 
   presets: [
@@ -145,6 +145,27 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+
+  plugins: [
+  [
+    '@docusaurus/plugin-content-docs',
+    {
+      id: 'projects',
+      path: 'projects',
+      routeBasePath: 'projects',
+      sidebarPath: './sidebars/projects.ts',
+    },
+  ],
+  [
+    '@docusaurus/plugin-content-docs',
+    {
+      id: 'tutorials',
+      path: 'tutorials',
+      routeBasePath: 'tutorials',
+      sidebarPath: './sidebars/tutorials.ts',
+    },
+  ],
+],
 };
 
 export default config;
